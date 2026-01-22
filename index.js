ca3883d322fdfe8d89ba3e8c7b749917671081b9
cokorie <chijioke.okorie@gmail.com>
2026-01-21 20:07:11 -0600

    add id to scoreText

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const gameBoard = document.querySelector("#board");
 const ctx = gameBoard.getContext("2d");
-const scoreText = document.querySelector("score");
+const scoreText = document.querySelector("#score");
 const resetBtn = document.querySelector("reset");
