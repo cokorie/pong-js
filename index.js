3e1da0e5462e16dd0e415b85f4dfc2e82504a90a
cokorie <chijioke.okorie@gmail.com>
2026-01-21 20:06:43 -0600

    add and define resetBtn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const gameBoard = document.querySelector("#board");
 const ctx = gameBoard.getContext("2d");
 const scoreText = document.querySelector("score");
+const resetBtn = document.querySelector("reset");
