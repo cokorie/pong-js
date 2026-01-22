286aaa29cd52a9732d96b3f91629c90de3a7dff1
cokorie <chijioke.okorie@gmail.com>
2026-01-21 20:08:13 -0600

    add and define gameWidth

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const gameBoard = document.querySelector("#board");
 const ctx = gameBoard.getContext("2d");
 const scoreText = document.querySelector("#score");
 const resetBtn = document.querySelector("#reset");
+const gameWidth = gameBoard.width;
