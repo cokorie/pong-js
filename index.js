f492d4d9b1237e0d4fef09738aeccc612bd68346
cokorie <chijioke.okorie@gmail.com>
2026-01-22 06:31:50 -0600

    fill paddle2 height

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function drawPaddles() {
     ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
 
     ctx.fillStyle = paddle2Color;
-    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width);
+    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 };
 
 function createBall() {
